700c1a69b0a72c58fafeee170db7fe39cbd3331d
Jin Li <lijin.abc@gmail.com>
2019-12-26 15:19:40 -0600

    add exitcode

---
+++ tests/exitcode/main.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# vim: set noexpandtab tabstop=2:
+
+set -v
+f=../LC1_chr_1k.bam
+pefiltertag -i "$f" -s -t 4
+../../src/bseqc2/bseqc2 -h
+echo $?
+tmpdir=$(mktemp -u -d)
+../../src/bseqc2/bseqc2 -i "$f" -l 160 -r /mnt/folders/resource/genome/hg38/fasta/hg38.fa -o "$tmpdir/result.txt"
+echo $?
+tree "$tmpdir"
